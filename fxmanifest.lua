7c33fec7bd3b6690ba15017ec33c147e9804a1c3
Noy Bueno <noybueno90@gmail.com>
2021-11-08 17:08:16 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 games { 'gta5' }
 
 author 'Noy Bueno (noyb051@gmail.com)'
-description 'Simple resources made in order to track staff members activity time'
+description 'Simple resource made in order to track staff members activity time'
 version '1.0b'
 
 server_scripts {
